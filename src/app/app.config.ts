0ec74149a24d2a5e462d51acb95830c3067bfe4e
Walther Zapata Casas <wartozc@gmail.com>
2025-05-31 19:28:38 -0500

    Add build production

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { UserGateway } from './domain/model/gateways/user-gateway';
 import { UserService } from './infrastructure/driven-adapters/user.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()),
-    {provide: UserGateway, useClass: UserService}],
+    {provide: UserGateway, useClass: UserService}, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 };
